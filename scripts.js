36eccf0c516358c1278c8feb13e3c872db97e035
Patryk Pijanowski <patryk.pijanowski@gdgpoland.org>
2020-07-14 00:13:46 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,48 @@
+function setCookie(name, value, days, path, domain, secure) {
+
+    if(!navigator.cookieEnabled) return;
+
+    var e = encodeURIComponent;
+
+    var cookie = e(name) + "=" + e(value);
+
+    if(typeof days === "number") {
+        var date = new Date();
+
+        date.setTime(date.getTime() + days * 1000 * 60 * 60 * 24);
+
+        cookie += "; expires=" + date.toGMTString();
+    }
+
+    if(path) {
+        cookie += "; path=" + path;
+    }
+
+    if(domain) {
+        cookie += "; domain=" + domain;
+    }
+
+    if(secure) {
+        cookie += "; secure";
+    }
+
+    document.cookie = cookie;
+
+}
+
+function getCookie(name) {
+
+    if(!document.cookie) return null;
+
+    var arr = document.cookie.split(/; */),
+        cookies = {};
+
+        arr.forEach(function(cookie) {
+            cookie = cookie.split("=");
+            cookies[cookie[0]] = decodeURIComponent(cookie[1]);
+        });
+
+    
+        return cookies[name] || null;
+
+}
